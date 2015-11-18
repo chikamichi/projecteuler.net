05570dc5f717f739684f4dad08487ac418ad1cf0
Jean-Denis Vauguet <jd@vauguet.fr>
2015-11-18 02:25:43 +0100

    Add a Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    "pkg": grunt.file.readJSON('package.json')
+  });
+
+  // grunt.registerTask("default", ["solve"]);
+};
